da46a964f312a0c959e2026dfd52da7e14ebb3e8
Robert Chonge <106912093+robertchonge@users.noreply.github.com>
2025-08-06 23:12:54 +0200

    Create create_clients.rb

---
+++ db/migrate/create_clients.rb
@@ -0,0 +1,10 @@
+class CreateClients < ActiveRecord::Migration[7.1]
+  def change
+    create_table :clients do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :email
+      t.timestamps
+    end
+  end
+end
